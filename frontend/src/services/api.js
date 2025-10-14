7613dec730772149ff617b72960014fe4a4bcf2c
GamySAF <gammiigamy@gmail.com>
2025-10-14 08:40:52 +0300

    fixing auth bugs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 // ✅ Create Axios instance with backend URL
 const API = axios.create({
-  baseURL: "https://ratenow.onrender.com", // Render backend URL
+  baseURL: "https://ratenow.onrender.com/", // Render backend URL
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 // ✅ Submit feedback
